26f4a3929287efe8f52f324ff8108e9b7530d0db
BreanaMarie <msbremarie@gmail.com>
2015-11-12 14:49:26 -0800

    finished views framing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-
   root to: "welcome#index"
-
-
-
-
+  
   resources :sessions
-
-
   resources :users, only: [:show, :new, :edit]
-  resources :posts, only: [:show, :new]
+  resources :posts, only: [:show, :new, :create, :destroy]
   resources :citys, only: [:index, :show]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20151112184825) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "location"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
