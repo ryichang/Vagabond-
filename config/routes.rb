4246c125a1b58a812ca14d4c43c45966864f3abf
ryichang <ryichang@gmail.com>
2015-11-12 17:08:10 -0800

    Login Logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
 
   root to: "welcome#index"
 
-
-
-
   resources :sessions
-
+  delete '/logout' => 'sessions#destroy'
 
   resources :users
   resources :posts
   resources :citys
+
 end
 
