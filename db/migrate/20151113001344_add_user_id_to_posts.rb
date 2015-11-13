52aab2e71326fa4823ff76bdb563f208f46ab7c8
BreanaMarie <msbremarie@gmail.com>
2015-11-12 18:56:29 -0800

    remove bad migrations

--- db/migrate/20151113001344_add_user_id_to_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddUserIdToPosts < ActiveRecord::Migration
-
-  change_table :posts do |t|
-
-      t.references :user
-  end
-end
--- db/migrate/20151113001738_add_city_id_to_posts.rb
+++
@@ -1,9 +0,0 @@
-class AddCityIdToPosts < ActiveRecord::Migration
-
-  change_table :posts do |t|
-
-      t.references :city
-  end
-
-
-end
