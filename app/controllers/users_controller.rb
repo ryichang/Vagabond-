c92d716a98c79ca740d1670370791560e0763698
ryichang <ryichang@gmail.com>
2015-11-12 19:24:32 -0800

    Seeded Posts and Show Post Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		render :show
+    p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!! UsersController #{params[:id]}"
 	end
 
 	def create
