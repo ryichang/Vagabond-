cab7f9a8e32ca81fa22e27b9175942b5cd4058ec
BreanaMarie <msbremarie@gmail.com>
2015-11-12 17:47:57 -0800

    updates to controllers and city index and post views

--- app/controllers/citys_controller.rb
+++ app/controllers/citys_controller.rb
@@ -1,2 +1,23 @@
 class CitysController < ApplicationController
+
+  def index
+    @city = City.all
+    render :index
+  end
+
+  def show
+    @city = City.find(params[:id])
+    render :show
+  end
+
+  def new
+    @city = City.new
+  end
+
+  def create
+    city_params = params.require(:city).permit(:name)
+    @city = City.create(city_params)
+    redirect_to @city # <-- go to city
+  end
+
 end
