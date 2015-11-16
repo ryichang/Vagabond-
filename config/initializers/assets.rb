43f56f5476b8fae4715a706b09e66530808d77a9
BreanaMarie <msbremarie@gmail.com>
2015-11-15 17:05:57 -0800

    add new post form to city/show page, additional change to stylesheets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,10 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+ Rails.application.config.assets.precompile += %w( 
+  citys.css
+  posts.css
+  sessions.css
+  users.css
+  welcome.css
+  )
